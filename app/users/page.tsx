8c61abd0e85de908a9f9b0156cb82e79f02bca75
Kiril-Pavlov <Kire_P@yahoo.com>
2023-10-21 23:31:28 +0200

    made a client component and fetched data

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const UsersPage = () => {
-  return (
-    <div>UsersPage</div>
-  )
+interface User {
+  id: number;
+  name: string;
 }
 
-export default UsersPage
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users:User[] = await res.json();
+  //   .then(response => response.json())
+  //   .then(json => console.log(json))
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => {
+          return <li key={user.id}>{user.name}</li>;
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default UsersPage;
